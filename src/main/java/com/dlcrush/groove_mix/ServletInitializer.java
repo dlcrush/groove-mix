8586aa0dfd374124a04737b051541845afe536cc
David Crush <davidcrushjr@gmail.com>
2024-07-20 23:00:51 -0400

    init

---
+++ src/main/java/com/dlcrush/groove_mix/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.dlcrush.groove_mix;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GrooveMixApplication.class);
+	}
+
+}
